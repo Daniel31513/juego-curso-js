b0d2dfa158e16540db9630ccb8bc1bb48bec50b1
Daniel Carrillo <danielcarrillo216@gmail.com>
2024-06-27 20:18:17 -0600

    clase 1

---
+++ app.js
@@ -0,0 +1,9 @@
+
+let numeroSecreto = 6;
+let numeroUsuario = prompt("indicame un numero");
+
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto){
+    alert ('acertaste el numero')
+}
